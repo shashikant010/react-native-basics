eaf533517926a254b301ffdc844d1eee3a8ef398
shashikant010 <shashikantyadav0503@gmail.com>
2024-04-26 21:57:32 +0530

    learning to deploy apps

--- passwordGenerator01/App.tsx
+++ passwordGenerator01/App.tsx
@@ -82,7 +82,6 @@ export default function App() {
        initialValues={{ passwordLength: '' }}
        validationSchema={PasswordSchema}
        onSubmit={ values => {
-        console.log(values);
         generatePasswordString(+values.passwordLength) 
        }}
      >
